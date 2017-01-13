18bbf45027207d9559c6e5d4e1b7f8da29cc7215
Andrey Dyachenko <winnts@gmail.com>
2017-01-13 18:46:29 +0200

    some changes

--- src/main/java/LinkedIn/LoginHTMLUnit.java
+++ src/main/java/LinkedIn/LoginHTMLUnit.java
@@ -4,6 +4,8 @@ import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
 
+import java.util.List;
+
 /**
  * Created by win on 11.01.17.
  */
@@ -12,24 +14,46 @@ public class LoginHTMLUnit {
         try {
             String url = "https://www.linkedin.com/uas/login?goback=&trk=hb_signin";
             final WebClient webClient = new WebClient();
-            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setJavaScriptEnabled(false);
             webClient.getOptions().setCssEnabled(false);
-//            webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
             final HtmlPage loginPage = webClient.getPage(url);
+//            webClient.waitForBackgroundJavaScript(10000);
+//            webClient.waitForBackgroundJavaScriptStartingBefore(10000);
             //Get Form By name
             final HtmlForm loginForm = loginPage.getFormByName("login");
             final HtmlSubmitInput button = loginForm.getInputByName("signin");
             final HtmlTextInput usernameTextField = loginForm.getInputByName("session_key");
             final HtmlPasswordInput passwordTextField = loginForm.getInputByName("session_password");
-            usernameTextField.setValueAttribute("");//your Linkedin Username
-            passwordTextField.setValueAttribute("");//Your Linkedin Password
+            usernameTextField.setValueAttribute("andrey.dyachenko@outlook.com");//your Linkedin Username
+            passwordTextField.setValueAttribute("GfHjkm777");//Your Linkedin Password
             final HtmlPage responsePage = button.click();
-            webClient.waitForBackgroundJavaScript(30000);
-            webClient.waitForBackgroundJavaScriptStartingBefore(30000);
-            String htmlBody = responsePage.getWebResponse().getContentAsString();
-            webClient.waitForBackgroundJavaScript(30000);
-            webClient.waitForBackgroundJavaScriptStartingBefore(30000);
-            System.out.println(htmlBody);
+            webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setCssEnabled(true);
+            String homeUrl = "https://www.linkedin.com/nhome/";
+            final HtmlPage homePage = webClient.getPage(homeUrl);
+//            webClient.waitForBackgroundJavaScript(10000);
+            webClient.waitForBackgroundJavaScriptStartingBefore(50000);
+
+            for (int i = 0; i < 20; i++) {
+                System.out.println("Condition: " + homePage.getElementById("ozfeed").isDisplayed());
+                if (homePage.getElementById("ozfeed").isDisplayed()) {
+                    break;
+                }
+                synchronized (homePage) {
+                    homePage.wait(500);
+                }
+            }
+            System.out.println("Element: " + homePage.getElementById("ozfeed"));
+            DomElement ozFeed = homePage.getElementById("ozfeed");
+            DomNode feedUpdates = ozFeed.getFirstChild().getFirstChild().getFirstChild();
+            System.out.println(feedUpdates.asText());
+
+
+
+            String htmlBody = homePage.getWebResponse().getContentAsString();
+//            System.out.println(htmlBody);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
